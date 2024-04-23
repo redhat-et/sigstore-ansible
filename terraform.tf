775493520df6ddb8e152254add2a90bf860ef684
Ryan Cook <rcook@redhat.com>
2024-04-23 19:08:31 -0400

    cred issue
    Signed-off-by: Ryan Cook <rcook@redhat.com>

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,12 @@ terraform {
     required_providers {
         aws = {
         source  = "hashicorp/aws"
-        version = "~> 5.6.2"
+        version = "~> 4.0"
         }
         null = {
         source  = "hashicorp/null"
         version = "~> 3.2.1"
         }
     }
+    required_version = ">= 0.14.9"
 }
